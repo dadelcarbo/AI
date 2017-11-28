523667449365b91513200ed0be7966a0672a524e
Dad El Carbo <david.caarbonel@free.fr>
2017-11-28 16:30:04 +0100

    UPdate my MNIST version

---
+++ MNIST/input_data.py
@@ -0,0 +1,98 @@
+﻿# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Functions for downloading and reading MNIST data."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import os
+import tempfile
+
+import numpy
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+
+
+def read_data_sets(train_dir,
+                   fake_data=False,
+                   one_hot=False,
+                   dtype=dtypes.float32,
+                   reshape=True,
+                   validation_size=5000,
+                   seed=None,
+                   source_url=DEFAULT_SOURCE_URL):
+  if fake_data:
+
+    def fake():
+      return DataSet(
+          [], [], fake_data=True, one_hot=one_hot, dtype=dtype, seed=seed)
+
+    train = fake()
+    validation = fake()
+    test = fake()
+    return base.Datasets(train=train, validation=validation, test=test)
+
+  if not source_url:  # empty string check
+    source_url = DEFAULT_SOURCE_URL
+
+  TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
+  TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
+  TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
+  TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
+
+  local_file = base.maybe_download(TRAIN_IMAGES, train_dir,
+                                   source_url + TRAIN_IMAGES)
+  with gfile.Open(local_file, 'rb') as f:
+    train_images = extract_images(f)
+
+  local_file = base.maybe_download(TRAIN_LABELS, train_dir,
+                                   source_url + TRAIN_LABELS)
+  with gfile.Open(local_file, 'rb') as f:
+    train_labels = extract_labels(f, one_hot=one_hot)
+
+  local_file = base.maybe_download(TEST_IMAGES, train_dir,
+                                   source_url + TEST_IMAGES)
+  with gfile.Open(local_file, 'rb') as f:
+    test_images = extract_images(f)
+
+  local_file = base.maybe_download(TEST_LABELS, train_dir,
+                                   source_url + TEST_LABELS)
+  with gfile.Open(local_file, 'rb') as f:
+    test_labels = extract_labels(f, one_hot=one_hot)
+
+  if not 0 <= validation_size <= len(train_images):
+    raise ValueError(
+        'Validation size should be between 0 and {}. Received: {}.'
+        .format(len(train_images), validation_size))
+
+  validation_images = train_images[:validation_size]
+  validation_labels = train_labels[:validation_size]
+  train_images = train_images[validation_size:]
+  train_labels = train_labels[validation_size:]
+
+
+  options = dict(dtype=dtype, reshape=reshape, seed=seed)
+
+  train = DataSet(train_images, train_labels, **options)
+  validation = DataSet(validation_images, validation_labels, **options)
+  test = DataSet(test_images, test_labels, **options)
+
+  return base.Datasets(train=train, validation=validation, test=test)
+
+
+
